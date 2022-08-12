e293b1e10a8a4412a617c6ddb9c14a2c031f84b9
TetianaAdamus <95863843+TetianaAdamus@users.noreply.github.com>
2022-08-12 23:14:20 +0300

    Update Lithuania.java

--- src/AntiRussianDefenceUnion/Lithuania.java
+++ src/AntiRussianDefenceUnion/Lithuania.java
@@ -1,4 +1,6 @@
 package AntiRussianDefenceUnion;
 
 public class Lithuania {
+  public static void main(String[] args) {
+        System.out.println("Alliance betwen Lithuania and Ukraine was the first step to create ARDU");
 }
