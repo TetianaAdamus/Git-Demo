a6070baa3dffb48c1b8d93498d96ef4c4ca15085
Tania Adamus <tania.adamus@gmail.com>
2022-08-19 19:40:45 +0300

    Brrrr

--- src/AntiRussianDefenceUnion/Poland.java
+++ src/AntiRussianDefenceUnion/Poland.java
@@ -1,7 +1,6 @@
 package AntiRussianDefenceUnion;
 
 public class Poland {
-    String toTestAgain;
     public void  PolandRoleInARDU() {
         System.out.println("Alliance betwen Poland and Ukraine was the first step to create ARDU");
     }
